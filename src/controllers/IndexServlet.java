52778e34d5f3e319362ce869b1e32d02aba1af82
User <User@DESKTOP-8KAK37H>
2023-05-22 15:30:45 +0900

    バリデーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -56,6 +56,7 @@ public class IndexServlet extends HttpServlet {
         request.setAttribute("messages", messages);
         request.setAttribute("messages_count", messages_count);     // 全件数
         request.setAttribute("page", page);                         // ページ数
+
         // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除）
         if(request.getSession().getAttribute("flush") != null) {
